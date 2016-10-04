1968a65fd904862dbf37e8f55a770e52f37ef859
memo <me@memo.blue>
2016-10-04 10:20:56 -0400

    added component scaffold and setup router

---
+++ src/components/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Order extends React.Component {
+  render() {
+    return (
+      <div>Inventory</div>
+    );
+  }
+}
+
+export default Order;
---
+++ src/components/StorePicker.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { getFunName } from '../helpers';
+
+class StorePicker extends React.Component {
+  goToStore(event) {
+    event.preventDefault();
+    // get text from input field
+    const storeId = this.storeInput.value;
+    // transition to store URL
+    this.context.router.transitionTo(`/store/${storeId}`);
+  }
+
+  render() {
+    return (
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
+        <h2>Please enter a store</h2>
+        <input ref={(el) => { this.storeInput = el }} type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <button type="submit">Visit Store</button>
+      </form>
+    );
+  }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
+}
+
+export default StorePicker;
