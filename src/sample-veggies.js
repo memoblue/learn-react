16e41329df6e6e67f19747a841a3c864c47fd954
memo <me@memo.blue>
2016-11-15 08:19:56 -0500

    veganize sample data

---
+++ src/sample-veggies.js
@@ -0,0 +1,74 @@
+// This is just some sample data so you don't have to think of your own!
+module.exports = {
+  veggie1: {
+    name: 'Tomatoes',
+    image: 'http://media.istockphoto.com/photos/fresh-tomatoes-on-wood-background-picture-id503022030',
+    desc: 'Everyones favorite veggie. We will cut it to the size you need and ship it.',
+    price: 1724,
+    status: 'available'
+  },
+
+  veggie2: {
+    name: 'Asparagus',
+    image: 'http://media.istockphoto.com/photos/green-asparagus-picture-id474395668',
+    desc: 'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
+    price: 3200,
+    status: 'available'
+  },
+
+  veggie3: {
+    name: 'Potatoes',
+    image: 'http://media.istockphoto.com/photos/roasted-potato-in-frying-pan-picture-id496876278',
+    desc: 'Big, sweet and tender. True potatoes from the beautiful fields of Ohio. About 8-10 per pound',
+    price: 1684,
+    status: 'unavailable'
+  },
+
+  veggie4: {
+    name: 'Avocados',
+    image: 'http://media.istockphoto.com/photos/fresh-avocado-picture-id187123951',
+    desc: 'Fatty flesh with a mild, sweet flavor profile, and moderately firm texture.',
+    price: 1129,
+    status: 'available'
+  },
+
+  veggie5: {
+    name: 'Onions',
+    image: 'http://media.istockphoto.com/photos/fresh-organic-onions-picture-id525245611',
+    desc: 'Crack these open and enjoy them plain or with one of our cocktail sauces.',
+    price: 4234,
+    status: 'available'
+  },
+
+  veggie6: {
+    name: 'Kale',
+    image: 'http://media.istockphoto.com/photos/kale-picture-id497951835',
+    desc: 'This green, juicy kale is truly the king of the field. Bake it, grill it, broil it… as good as it gets!',
+    price: 1453,
+    status: 'available'
+  },
+
+  veggie7: {
+    name: 'Brocolli',
+    image: 'http://media.istockphoto.com/photos/raw-broccoli-on-wooden-background-picture-id471153710',
+    desc: 'A firm plump brocolli with a sweet salty flavor and a clean finish.',
+    price: 2543,
+    status: 'available'
+  },
+
+  veggie8: {
+    name: 'Red Peppers',
+    image: 'http://media.istockphoto.com/photos/red-peppers-picture-id172238110',
+    desc: 'The best peppers from the Atlantic Northeast with a full-flavored and complex taste.',
+    price: 425,
+    status: 'available'
+  },
+
+  veggie9: {
+    name: 'Shiitake Mushrooms',
+    image: 'http://media.istockphoto.com/photos/shiitake-mushroom-on-wooden-table-picture-id538653407',
+    desc: 'With 21-25 two bite shiitake in each pound, these sweet morsels are perfect for a nabemono.',
+    price: 2250,
+    status: 'available'
+  }
+};
